ea6cade2336e723163f81517fa00e23f89378409
Oskari Rauta <oskari.rauta@gmail.com>
2022-03-31 22:47:37 +0300

    update logread example
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- examples/logread_main.cpp
+++ examples/logread_main.cpp
@@ -5,11 +5,10 @@
 #include <signal.h>
 #include <sys/wait.h>
 
+#include "logger.hpp"
 #include "logreader/logread.hpp"
 
-logreader::logread syslog;
-// to test singleton forcing:
-//logreader::syslog syslog2;
+logreader::logread logread;
 
 static struct sigaction exit_action, ignore_action/*, panic_action*/;
 static int stopped = 0;
@@ -17,37 +16,22 @@ static int stopped = 0;
 static void die_handler(int signum) {
 
 	std::cout << "received TERM signal [" << signum << "]" << std::endl;
-	syslog.sig_exit(); // ask syslog to quit
+	logread.sig_exit(); // ask syslog to quit
 	stopped = 1;
 	std::cout << "sig exit sent" << std::endl;
 }
-
-/*
-// not necessary or even useful..
-static void panic_handler(int signum) {
-	std::cout << "panic - kill child procs" << std::endl;
-	syslog.panic();
-}
-*/
 
 void add_signal_handler(void) {
 
 	exit_action.sa_handler = die_handler;
 	sigemptyset(&exit_action.sa_mask);
 	exit_action.sa_flags = 0;
-	// we handle SA_NOCLDWAIT on logreader::syslog for now
-	//exit_action.sa_flags = SA_NOCLDWAIT;
 
 	ignore_action.sa_handler = SIG_IGN;
 	sigemptyset(&ignore_action.sa_mask);
 	ignore_action.sa_flags = 0;
-/*
-	panic_action.sa_handler = panic_handler;
-	sigemptyset(&panic_action.sa_mask);
-	panic_action.sa_flags = 0;
-*/
+
 	sigaction(SIGTERM, &exit_action, NULL);
-//	sigaction(SIGALRM, &panic_action, NULL);
 	sigaction(SIGALRM, &ignore_action, NULL);
 	sigaction(SIGHUP, &ignore_action, NULL);
 	sigaction(SIGINT, &exit_action, NULL);
@@ -55,45 +39,74 @@ void add_signal_handler(void) {
 	sigaction(SIGQUIT, &ignore_action, NULL);
 	sigaction(SIGUSR1, &ignore_action, NULL);
 	sigaction(SIGUSR2, &ignore_action, NULL);
-//	sigaction(17, &exit_action, NULL);
 }
 
-int main() {
+static void usage(std::string name) {
+
+	std::cout << "\nusage: " <<
+		name.substr(name.find_last_of("/\\") + 1) <<
+		" [options]\n\n" <<
+		"options:\n" <<
+		"\t--help  | --h | -h: display help\n" <<
+		"\t--debug | --d | -d: enable debug output\n" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+
+	std::string app_name(argv[0]);
+	int x = 0;
+	bool debug = false;
 
 	add_signal_handler();
 
-	int x = 0;
+	for ( int i = 1; i < argc; i++ ) {
 
-	while ( !syslog.aborted()) {
+		std::string arg(argv[i]);
+		if ( arg == "-h" || arg == "--h" || arg == "--help" || arg == "-help" ) {
+
+			usage(app_name);
+			return 0;
+		} else if ( arg == "-d" || arg == "--d" || arg == "--debug" || arg == "-debug" )
+			debug = true;
+		else {
+
+			std::cout << "\nUnknown arg: " << arg << std::endl;
+			usage(app_name);
+			return -1;
+		}
+	}
+
+	if ( debug ) {
+
+		std::cout << std::endl;
+		logger::output_level[logger::verbose] = true;
+		logger::output_level[logger::vverbose] = true;
+		logger::output_level[logger::debug] = true;
+
+		logger::debug << "debug output enabled." << std::endl;
+		std::cout << std::endl;
+
+		// Displays messages is previous processes were killed during init
+
+		std::vector<logger::entry> hist = logger::last(0);
+		for ( auto it = hist.begin(); it != hist.end(); ++it )
+			std::cout << it -> msg << std::endl;
+	}
+
+	while ( !logread.aborted() || logread.running()) {
+
 		std::this_thread::sleep_for(std::chrono::milliseconds(50));
-		syslog.tail();
+		logread.tail();
+		logread.iterate([](auto name, auto txt) { std::cout << txt << std::endl; });
 
-		syslog.mutex.lock();
-		while ( syslog.entries.size() > 0 ) {
-			std::cout << "recv: \"" << syslog.entries.front() << "\"" << std::endl;
-			syslog.entries.pop_front();
-		}
-		syslog.mutex.unlock();
-
-		if ( stopped > 0 && stopped < 10 ) {
-			std::cout << "* * * stop * * *" << std::endl;
-			stopped++;
-		}
 		x++;
 		if ( x == 2553500 ) {
 			std::cout << "quit" << std::endl;
-			syslog.sig_exit(); // ask to abort
+			logread.sig_exit(); // ask to abort
 		}
 	}
 
-	std::cout << "stopping tailing" << std::endl;
-
-	while ( syslog.running()) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(80));
-		syslog.tail();
-	}
-
-	std::cout << "tailing ended" << std::endl;
+	std::cout << "logread tailing has ended" << std::endl;
 
 	return 0;
 }
